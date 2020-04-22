04b44a9379edd6d322ee56ea35455592222030cb
actonDev <chris.actondev@gmail.com>
2020-04-22 22:05:58 +0200

    yay! can filter

--- examples/hound_read.rs
+++ examples/hound_read.rs
@@ -1,4 +1,6 @@
+use dsp_playground::biquad;
 use hound;
+use std::i16;
 
 fn main() {
     let mut reader = hound::WavReader::open("assets/white_noise_mono.wav").unwrap();
@@ -24,10 +26,35 @@ fn main() {
     };
 
     let mut writer = hound::WavWriter::create("assets/write.wav", spec).unwrap();
-    for s in samples_vec {
-        writer.write_sample((s as f32 * 0.1) as i16).unwrap();
+    for s in &samples_vec {
+        writer.write_sample((*s as f32 * 0.1) as i16).unwrap();
     }
     writer.finalize().unwrap();
 
     println!("count {}", count);
+
+    filter_file(&samples_vec);
+}
+
+fn filter_file(samples: &Vec<i16>) {
+    let biquad_params = biquad::LOWPASS_FC_1000_Q_0_7071_GAIN_6;
+    let mut biquad_samples: biquad::BiquadSamples = Default::default();
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create("assets/filtered.wav", spec).unwrap();
+
+    for s in samples {
+        let s_float = (*s as f64) / i16::MAX as f64;
+        biquad_samples.sin = s_float;
+        let sout = biquad::process(&biquad_params, &mut biquad_samples);
+        let sout_int = (sout * (i16::MAX as f64)) as i16;
+        // println!("s {} sfloat {}", s, s_float);
+        // println!("  sout {} sout_int {}", sout, sout_int);
+        writer.write_sample(sout_int).unwrap();
+    }
 }
--- src/biquad.rs
+++ src/biquad.rs
@@ -1,21 +1,22 @@
-use std::any::type_name;
-
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
-
 pub struct Biquad {
-    a0: f64,
-    a1: f64,
-    a2: f64,
-    b1: f64,
-    b2: f64,
+    pub a0: f64,
+    pub a1: f64,
+    pub a2: f64,
+    pub b1: f64,
+    pub b2: f64,
 }
+
+pub const LOWPASS_FC_1000_Q_0_7071_GAIN_6: Biquad = Biquad {
+    a0: 0.00460399444634034,
+    a1: 0.00920798889268068,
+    a2: 0.00460399444634034,
+    b1: -1.7990948352036205,
+    b2: 0.8175108129889816,
+};
 
 #[derive(Default)]
 pub struct BiquadSamples {
-    sout: f64,
-    sin: f64,
+    pub sin: f64,
     // past samples
     sin_1: f64,
     sin_2: f64,
@@ -35,20 +36,24 @@ impl Default for Biquad {
     }
 }
 
-pub fn process(biquad: &Biquad, samples: &mut BiquadSamples){
+pub fn process(biquad: &Biquad, samples: &mut BiquadSamples) -> f64 {
     let direct = samples.sin * biquad.a0;
+
     let forw_1 = samples.sin_1 * biquad.a1;
     let forw_2 = samples.sin_2 * biquad.a2;
+
     let bakw_1 = - samples.sout_1 * biquad.b1;
     let bakw_2 = - samples.sout_2 * biquad.b2;
 
     let out = direct + forw_1 + forw_2 + bakw_1 + bakw_2;
 
+    // temp
+    // let out = direct;
     
     samples.sin_2 = samples.sin_1;
     samples.sin_1 = samples.sin;
 
     samples.sout_2 = samples.sout_1;
-    samples.sout_1 = samples.sout;
-    samples.sout = out;
+    samples.sout_1 = out;
+    out
 }
